5c0318e0906a55a00bd8d1a34445b50b3d362b1c
TimWeyel <tweyel@gmail.com>
2021-04-29 15:36:13 -0700

    added Product.destroy endpoint

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -139,6 +139,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }    
+  })
+  .then(dbProductData => {
+    if(!dbProductData) {
+      res.status(404).json({ message: 'No product found with this id'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
