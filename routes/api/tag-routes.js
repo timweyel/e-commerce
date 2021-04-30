d057aeda881bb2a19f5813695e55118c1c292e54
TimWeyel <tweyel@gmail.com>
2021-04-30 12:43:42 -0700

    added Tag.update and Tag.destroy routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,10 +61,40 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if(!dbTagData[0]) {
+        res.status(404).json({ message: 'No tag found with that ID'});
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if(!dbTagData) {
+        res.status(404).json({ message: 'No tag found for that ID'});
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
