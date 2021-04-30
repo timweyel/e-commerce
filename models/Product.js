0ee27e15957af802b9c4008ccb9c31aff2dd672c
TimWeyel <tweyel@gmail.com>
2021-04-30 11:50:57 -0700

    forgot to add .env file

--- models/Product.js
+++ models/Product.js
@@ -10,37 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     product_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     price: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-    validate: {
-      isDecimal: true
-    }
-  },
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
     stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    default: 10,
-    validate: {
-      isNumberic: true
-    }
-  },
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
     category_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'category',
-      key: 'id'
-    }
+      type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      }
   },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
