c430c82c9e9c39952e81fad2c963fff979095f58
TimWeyel <tweyel@gmail.com>
2021-04-30 11:10:12 -0700

    fixed Product model

--- models/Product.js
+++ models/Product.js
@@ -9,38 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: DataTypes.INTEGER,
+    id: {
+    type: DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
     autoIncrement: true
   },
-  {
-    product_name: DataTypes.STRING,
+    product_name: {
+    type: DataTypes.STRING,
     allowNull: false,
   },
-  {
-    price: DataTypes.DECIMAL,
+    price: {
+    type: DataTypes.DECIMAL,
     allowNull: false,
     validate: {
       isDecimal: true
     }
   },
-  {
-    stock: DataTypes.INTEGER,
+    stock: {
+    type: DataTypes.INTEGER,
     allowNull: false,
     default: 10,
     validate: {
       isNumberic: true
     }
   },
-  {
-    category_id: DataTypes.INTEGER,
+    category_id: {
+    type: DataTypes.INTEGER,
     references: {
       model: 'category',
       key: 'id'
     }
   },
-  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
